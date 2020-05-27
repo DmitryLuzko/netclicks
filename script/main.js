a07e436c6d935f2b720495a6037e1799c88c66d5
d10 <d10.luzko@gmail.com>
2020-05-27 14:48:52 +0300

    netclicks day2

--- script/main.js
+++ script/main.js
@@ -1,6 +1,59 @@
-// получение кнопок меню
-const leftMenu = document.querySelector('.left-menu');
-const hamburger = document.querySelector('.hamburger');
+const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2',
+  API_KEY = '705e304a1444804fc433c2e12ba7b051';
+
+// получение элементов страницы
+const leftMenu = document.querySelector('.left-menu'),
+  hamburger = document.querySelector('.hamburger'),
+  tvShowsList = document.querySelector('.tv-shows__list'),
+  modal = document.querySelector('.modal');
+
+const DBService = class { //создание карточек
+  getData = async (url) => {
+    const res = await fetch(url);
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error(`Не удалось получить данные по адресу ${url}`)
+    }
+
+  }
+
+  getTestData = async () => {
+    return await this.getData('test.json')
+  }
+}
+
+const renderCard = response => {
+  console.log(response);
+  tvShowsList.textContent = '';
+
+  response.results.forEach(({backdrop_path: backdrop,
+                            name: title,
+                            poster_path: poster,
+                            vote_average: vote
+  }) => {
+    const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
+    const backdropIMG = backdrop ? `data-backdrop="${IMG_URL}${ backdrop }"` : null;
+    const voteElem = vote ? `<span class="tv-card__vote">${vote}</span>` : '';
+
+    const card = document.createElement('li');
+    card.className = 'tv-shows__item';
+    card.innerHTML = `
+      <a href="#" class="tv-card">
+          ${voteElem}
+          <img class="tv-card__img"
+               src="${posterIMG}"
+               ${backdropIMG}"
+               alt="${title}">
+          <h4 class="tv-card__head">${title}</h4>
+      </a>
+    `;
+    tvShowsList.append(card);
+  });
+};
+
+new DBService().getTestData().then(renderCard);
+
 
 // открытие/закрытие меню
 
@@ -9,7 +62,8 @@ hamburger.addEventListener('click', event => {
   hamburger.classList.toggle('open');
 });
 
-document.addEventListener('click', event => { //закрытие меню при клике не в меню
+//закрытие меню при клике не в меню
+document.addEventListener('click', event => {
   const target = event.target;
   if (!target.closest('.left-menu')) {
     leftMenu.classList.remove('openMenu');
@@ -18,7 +72,8 @@ document.addEventListener('click', event => { //закрытие меню при
   }
 });
 
-leftMenu.addEventListener('click', event => { //открытие меню при клике на иконку, а не на гамбургер
+//открытие меню при клике на иконку, а не на гамбургер
+leftMenu.addEventListener('click', event => {
   const target = event.target;
   const dropdown = target.closest('.dropdown')
   if (dropdown) {
@@ -28,13 +83,37 @@ leftMenu.addEventListener('click', event => { //открытие меню при
   }
 });
 
-const tvImg = document.querySelectorAll('.tv-card__img') //изменение картинки фильма при наведении
-for (let i = 0; i < tvImg.length; i++) {
-    let originalImage = tvImg[i].src;
-    tvImg[i].addEventListener('mouseenter', event => {
-        tvImg[i].src = tvImg[i].getAttribute("data-backdrop");
-    });
-    tvImg[i].addEventListener('mouseout', event => {
-        tvImg[i].src = originalImage;
-    });
+//изменение картинки фильма при наведении
+const changeImg = event => {
+  const card = event.target.closest('.tv-shows__item');
+  if (card) {
+    const img = card.querySelector('.tv-card__img');
+    if (img.dataset.backdrop) {
+      [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src];
+    }
+    img.dataset.max = '123';
+  }
 };
+
+tvShowsList.addEventListener('mouseover', changeImg);
+tvShowsList.addEventListener('mouseout', changeImg);
+
+//открытие модального окна
+tvShowsList.addEventListener('click', event => {
+  event.preventDefault();
+  const target = event.target;
+  const card = target.closest('.tv-card');
+  if (card) {
+    document.body.style.overflow = 'hidden';
+    modal.classList.remove('hide');
+  };
+});
+
+// закрытие модального окна
+modal.addEventListener('click', event => {
+  if (event.target.closest('.cross') ||
+    event.target.classList.contains('modal')) {
+    document.body.style.overflow = '';
+    modal.classList.add('hide');
+  };
+})
