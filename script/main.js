24d71ffc7375be67f9f9b59876988b166bc19d97
d10 <d10.luzko@gmail.com>
2020-05-28 12:44:15 +0300

    day3

--- script/main.js
+++ script/main.js
@@ -1,13 +1,27 @@
 const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2',
+  SERVER = 'https://api.themoviedb.org/3'
   API_KEY = '705e304a1444804fc433c2e12ba7b051';
 
 // получение элементов страницы
 const leftMenu = document.querySelector('.left-menu'),
   hamburger = document.querySelector('.hamburger'),
   tvShowsList = document.querySelector('.tv-shows__list'),
-  modal = document.querySelector('.modal');
+  modal = document.querySelector('.modal'),
+  tvShows = document.querySelector('.tv-shows'),
+  tvCardImg = document.querySelector('.tv-card__img'),
+  modalTitle = document.querySelector('.modal__title'),
+  rating = document.querySelector('.rating'),
+  genresList = document.querySelector('.genres-list'),
+  description = document.querySelector('.description'),
+  modalLink = document.querySelector('.modal__link'),
+  searchForm = document.querySelector('.search__form')
+  searchFormInput = document.querySelector('.search__form-input')
 
-const DBService = class { //создание карточек
+const loading = document.createElement('div');
+loading.className = 'loading';
+
+//создание карточек
+const DBService = class {
   getData = async (url) => {
     const res = await fetch(url);
     if (res.ok) {
@@ -15,31 +29,36 @@ const DBService = class { //создание карточек
     } else {
       throw new Error(`Не удалось получить данные по адресу ${url}`)
     }
-
   }
 
-  getTestData = async () => {
-    return await this.getData('test.json')
-  }
+  getTestData = () => this.getData('test.json');
+
+  getTestCard = () => this.getData('card.json');
+
+  getSearchResult = query => this.getData(`${SERVER}/search/tv?api_key=${API_KEY}&query=${query}&language=ru-RU`);
+
+  getTvShow = id => this.getData(`${SERVER}/tv/${id}?api_key=${API_KEY}&language=ru-RU`);
 }
 
 const renderCard = response => {
-  console.log(response);
   tvShowsList.textContent = '';
 
-  response.results.forEach(({backdrop_path: backdrop,
-                            name: title,
-                            poster_path: poster,
-                            vote_average: vote
+  response.results.forEach(({
+    backdrop_path: backdrop,
+    name: title,
+    poster_path: poster,
+    vote_average: vote,
+    id
   }) => {
     const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
-    const backdropIMG = backdrop ? `data-backdrop="${IMG_URL}${ backdrop }"` : null;
+    const backdropIMG = backdrop ? `data-backdrop="${IMG_URL}${backdrop}"` : null;
     const voteElem = vote ? `<span class="tv-card__vote">${vote}</span>` : '';
 
     const card = document.createElement('li');
+    card.idTv = id;
     card.className = 'tv-shows__item';
     card.innerHTML = `
-      <a href="#" class="tv-card">
+      <a href="#" id = ${id} class="tv-card">
           ${voteElem}
           <img class="tv-card__img"
                src="${posterIMG}"
@@ -48,15 +67,28 @@ const renderCard = response => {
           <h4 class="tv-card__head">${title}</h4>
       </a>
     `;
+
     tvShowsList.append(card);
   });
+
+  if (!response.results.length) {
+      tvShowsList.innerHTML = `<p>Такого сериала не существует, введите корректное название</p>`;
+  }
+
+  loading.remove();
 };
 
-new DBService().getTestData().then(renderCard);
-
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const value = searchFormInput.value.trim();
+  searchFormInput.value = '';
+  if (value) {
+    tvShows.append(loading);
+    new DBService().getSearchResult(value).then(renderCard);
+  }
+});
 
 // открытие/закрытие меню
-
 hamburger.addEventListener('click', event => {
   leftMenu.classList.toggle('openMenu');
   hamburger.classList.toggle('open');
@@ -68,12 +100,12 @@ document.addEventListener('click', event => {
   if (!target.closest('.left-menu')) {
     leftMenu.classList.remove('openMenu');
     hamburger.classList.remove('open');
-    console.log(event);
   }
 });
 
 //открытие меню при клике на иконку, а не на гамбургер
 leftMenu.addEventListener('click', event => {
+  event.preventDefault();
   const target = event.target;
   const dropdown = target.closest('.dropdown')
   if (dropdown) {
@@ -104,8 +136,26 @@ tvShowsList.addEventListener('click', event => {
   const target = event.target;
   const card = target.closest('.tv-card');
   if (card) {
-    document.body.style.overflow = 'hidden';
-    modal.classList.remove('hide');
+
+    new DBService().getTvShow(card.id)
+      .then(data => {
+        console.log(data);
+        tvCardImg.src = IMG_URL + data.poster_path;
+        modalTitle.textContent = data.name;
+        genresList.textContent = '';
+        for (const item of data.genres) {
+          genresList.innerHTML += `<li>${item.name}</li>`
+        };
+        rating.textContent = data.vote_average;
+        description.textContent = data.overview;
+        modalLink.href = data.homepage;
+      })
+
+      .then(() => {
+        document.body.style.overflow = 'hidden';
+        modal.classList.remove('hide');
+      })
+
   };
 });
 
